1a8539e947c186a2869d41a83c4ea3b65f495fb1
Helena Franczak <helena.franczak@gmail.com>
2018-04-17 12:32:16 +1000

    Update main.js
    choices are displayed

--- js/main.js
+++ js/main.js
@@ -37,66 +37,95 @@ package: {choice: 'Not Selected', price: 0}
 
 
 $('ul li').on('click', function(e){
-	e.preventDefault();
 
-	$('li.active').removeClass('active');
+	   e.preventDefault();
 
-$(this).addClass('active');
+	   $('li.active').removeClass('active');
+
+      $(this).addClass('active');
+
+      $('#options-display').html('');
+
+     // var source = $(this).html();
 
 
-$('#options-display').html('');
 
 switch($(this).data('tab')){
 
-    case 'vehicle':
+  case 'vehicle':
+
+        var source = $('#vehicle-options-template').html(); 
+
+        var template = Handlebars.compile(source);
+
+            for( var i=0; i<carOption.length; i++){
+
+            var data = {feature: carOption[i].choice, price: carOption[i].price};
+
+            //console.log(carOption[i].choice, carOption[i].price); // it displays all objects and price
+
+            var newListItemHTML = template(data);
+
+              console.log(newListItemHTML); //displays all objects
+               $('#options-display').append(newListItemHTML);
+
+          };
 
 
-//grab handlebars template
-var source = $('#vehicle-options-template').html(); 
-//compile source
+       break;
 
-var template = Handlebars.compile(source);
+  case 'color':
+        var source = $('#color-options-template').html(); 
 
-//create object literal
+        var template = Handlebars.compile(source);
 
-for( var i=0; i<carOption.length; i++){
+            for( var j=0; j<colorOption.length; j++){
 
-  var data = {feature: carOption[i].choice, price: carOption[i].price};
+            var data = {feature: colorOption[j].choice, price: colorOption[j].price};
 
-  console.log(carOption[i].choice, carOption[i].price); // it displays all objects and price
+            //console.log(carOption[i].choice, carOption[i].price); // it displays all objects and price
+
+            var newListItemHTML = template(data);
+
+              console.log(newListItemHTML); //displays all objects
+              $('#options-display').append(newListItemHTML);
+
+          };
+
+        break;
+  case 'package':
+          var source = $('#package-options-template').html(); 
+
+          var template = Handlebars.compile(source);
+
+            for( var k=0; k<packageOptions.length; k++){
+
+            var data = {feature: packageOptions[k].choice, price: packageOptions[k].price};
+
+            //console.log(carOption[i].choice, carOption[i].price); // it displays all objects and price
+
+            var newListItemHTML = template(data);
+
+              console.log(newListItemHTML); //displays all objects
+
+              $('#options-display').append(newListItemHTML);
+
+          };
+
+
+        //code 
+        break;
+  case 'summary':
+
+
+
+        //code 
+        break;
+  default:
+        break;
+
 
 };
-
-    var newListItemHTML = template(data);
-
-    console.log(newListItemHTML); //displays only last object 'soul'
-
-
-//define and add data to template
-
-		$('#options-display').html(newListItemHTML);
-
-
-        break;
-    case 'color':
-        //code 
-        break;
-        case 'package':
-        //code 
-        break;
-        case 'summary':
-        //code 
-        break;
-    default:
-        break;
-};
-
-
-
-// //-// add the new content to the DOM
-// $('#packingList').append(newListItemHTML);
-// });
-
 
 
 });
